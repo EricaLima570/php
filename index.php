5d278db88a88fccecd07f18f6b3bae552aa7ed11
Erica Lima <ericalimasilva570@gmail.com>
2023-04-20 19:25:41 -0300

    Adicionando form e indice  no front do exercicio 3

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
         <li><a href="./exercicio2/" target="_blank">Exercicio 2</a></li>
         <li><a href="./exercicio3/" target="_blank">Exercicio 3</a></li>
         <li><a href="./exercicio4/" target="_blank">Exercicio 4</a></li>
-    </ul>    
+    </ul>   
 </body>
 </html>
+
+
+
