4e5b762ea2efa0bc98afea13899343ff55264b1c
Erica Lima <ericalimasilva570@gmail.com>
2023-04-26 19:56:50 -0300

    Adicionando form do front no exercicio 4

---
+++ exercicio4/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=drive-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+	<title> Exercicio 4 </title>
+</head>
+<body>
+	<form action="" method="post">
+        <h2>Exercicio 3</h2>
+        <br>
+        <div>
+            <label for="value1">Digite o primeiro valor</label>
+            <input type="number" name="value1">
+        </div>
+        <div>
+            <label for="value2">Digite o segundo valor</label>
+            <input type="number" name="value2">
+        </div>
+        <div>
+            <label for="value3">Digite o terceiro valor</label>
+            <input type="number" name="value3">
+        </div>
+        <input type="submit" value="Enviar">       
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html >
+<head>
+
+</head>
+<body>
+    <ul>
+        <li><a href="./exercicio1/" target="_blank">Exercicio 1</a></li>
+        <li><a href="./exercicio2/" target="_blank">Exercicio 2</a></li>
+        <li><a href="./exercicio3/" target="_blank">Exercicio 3</a></li>
+        <li><a href="./exercicio4/" target="_blank">Exercicio 4</a></li>
+    </ul>
+    
+</body>
+</html>
