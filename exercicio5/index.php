e9c02ed78ad048d43fa6e2530a671984d0568e54
Erica Lima <ericalimasilva570@gmail.com>
2023-05-02 21:33:27 -0300

    Adicionando form no front do exe 5

---
+++ exercicio5/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=drive-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+	<title> Exercicio 5 </title>
+</head>
+<body>
+	<form action="" method="post">
+        <h2>Exercicio 3</h2>
+        <br>
+        <div>
+            <label for="value1">lado 1 do triangulo</label>
+            <input type="number" name="value1">
+        </div>
+        <div>
+            <label for="value2">lado 2 do triangulo</label>
+            <input type="number" name="value2">
+        </div>
+        <div>
+            <label for="value3">lado 3 do triangulo</label>
+            <input type="number" name="value3">
+        </div>
+        <input type="submit" value="Enviar">       
+    </form>
+</body>
+</html>
