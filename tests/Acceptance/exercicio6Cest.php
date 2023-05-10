9116511bec45875ff60c205d5165ffcd25242bdf
Erica Lima <ericalimasilva570@gmail.com>
2023-05-10 11:41:29 -0300

    adicionando o teste do title exercicio 6

--- tests/Acceptance/exercicio6Cest.php
+++ tests/Acceptance/exercicio6Cest.php
@@ -12,7 +12,44 @@ class exercicio6Cest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function TitleExercicio6Test(AcceptanceTester $I)
     {
+        //Eu estou na página "exercicio 6"
+        $I->amOnPage('/exercicio6');
+
+        //Eu vejo o titulo "Exercicio 2"
+        //Eu vejo "Exercicio 6" em h1
+        $I->seeInTitle('Exercício 6');
+    }
+    public function formExercicio6Test(AcceptanceTester $I)
+    {
+        /**
+         * Ler um número inteiro entre 1 e 12 e escrever
+         * o mês correspondente. Caso o número seja
+         * fora desse intervalo, informar que não existe
+         * mês com este número
+         */
+        $I->amOnPage('/exercicio6');
+
+        //criando array associativo em php para os meses:
+         $indicemes=array(  
+            1 => 'Janeiro',
+            2 => 'Fevereiro',
+            3 => 'Março',
+            4 => 'Abril',
+            5 => 'Maio',
+            6 => 'Junho',
+            7 => 'Julho',
+            8 => 'Agosto',
+            9 => 'Setembro',
+            10 =>'Outubro',
+            11 =>'Novembro',
+            12 =>'Dezembro',
+        );
+        //função para percorrer os vaores do array mes
+        foreach ($indicemes as $meses){
+            $I->fillField('meses', '$value1');
+            $I->click('Enviar');
+    }
     }
 }
