648d1e036ed92251f640913d682515b68e26a35b
Erica Lima <ericalimasilva570@gmail.com>
2023-05-10 11:44:32 -0300

    Criando a pasta com o form do exercicio 6

---
+++ exercicio6/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio1/style.css">
+    <title>Exercício 6</title>
+</head>
+<body>
+    <h1>Exercício 6</h1>
+    <form action="/exercicio1" method="post">
+    <div>
+        <label for="value1">Digite um valore entre 1 e 12</label>
+        <input type="number" name="value1">
+        <input type="submit" value="Enviar">  
+
+
+    </div>
+    </form>
+    
+</body>
+</html>
