46011aceb39c4d3182d601fba766ed4cd87dfe78
Erica Lima <ericalimasilva570@gmail.com>
2023-07-10 11:38:24 -0300

    Resolvendo conflitos

--- exercicio4/index.php
+++ exercicio4/index.php
@@ -13,17 +13,56 @@
         <br>
         <div>
             <label for="value1">Digite o primeiro valor</label>
-            <input type="number" name="value1">
+            <input required type="number" name="value1">
         </div>
         <div>
             <label for="value2">Digite o segundo valor</label>
-            <input type="number" name="value2">
+            <input required type="number" name="value2">
         </div>
         <div>
             <label for="value3">Digite o terceiro valor</label>
-            <input type="number" name="value3">
+            <input required type="number" name="value3">
         </div>
         <input type="submit" value="Enviar">       
     </form>
+    <?php
+    if (isset( $_POST["value1"])){
+        $value1 = $_POST['value1'];
+        $value2 = $_POST['value2'];
+        $value3 = $_POST['value3'];
+        if ($value1 >= $value2 && $value1 >= $value3) {
+            if ($value2 >= $value3) {
+                $first = $value1;
+                $second = $value2;
+                $third = $value3;
+            } else {
+                $first = $value1;
+                $second = $value3;
+                $third = $value2;
+            }
+        } elseif ($value2 >= $value1 && $value2 >= $value3) {
+            if ($value1 >= $value3) {
+                $first = $value2;
+                $second = $value1;
+                $third = $value3;
+            } else {
+                $first = $value2;
+                $second = $value3;
+                $third = $value1;
+            }
+        } else {
+            if ($value1 >= $value2) {
+                $first = $value3;
+                $second = $value1;
+                $third = $value2;
+            } else {
+                $first = $value3;
+                $second = $value2;
+                $third = $value1;
+            }
+        }
+        echo "Ordem decrescente: $first, $second, $third";
+    }
+    ?>
 </body>
 </html>
