335f0fe3c56d9eec254ae1837e2b9c4ced3fa36a
Erica Lima <ericalimasilva570@gmail.com>
2023-09-01 18:57:33 -0300

    resolvendo conflito no index do  exercicio 5

--- exercicio5/index.php
+++ exercicio5/index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=drive-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title> Exercicio 5 </title>
+	<title> Exercicio 5 </title>
 </head>
 <body>
-    <form action="" method="post">
+	<form action="/exercicio5/index.php" method="post">
         <h2>Exercicio 5</h2>
         <br>
         <div>
