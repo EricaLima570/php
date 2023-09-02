5974e73487b8f5dd957582d76ca611e740ed55aa
Erica Lima <ericalimasilva570@gmail.com>
2023-09-01 23:54:34 -0300

    Inserindo teste e back no exercicio 9

--- exercicio9/index.php
+++ exercicio9/index.php
@@ -9,9 +9,23 @@
 </head>
 <body>
 	<form action="index.php" method="post">
-        <h2>Quantas vezes você deseja imprimir a palavra SOL?</h2>
-        <input type="text" name="countRepeat" id="countRepeat">
-        <input type="submit" value="Enviar">
+        <label for="quantidade">Digite o número de vezes que deseja imprimir a palavra "SOL":</label>
+        <input type="number" id="quantidade" name="quantidade" min="1" required>
+        <button type="submit">Enviar</button>
     </form>
+    <?php
+    if (isset($_POST["quantidade"])) {
+        $quantidade = intval($_POST["quantidade"]);
+
+        if ($quantidade >= 1) {
+            echo "<h2>Resultado:</h2>";
+            for ($i = 0; $i < $quantidade; $i++) {
+                echo "SOL ";
+            }
+        } else {
+            echo "<p>O número deve ser maior ou igual a 1.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
--- tests/Acceptance/exercicio9Cest.php
+++ tests/Acceptance/exercicio9Cest.php
@@ -21,4 +21,22 @@ class Exercicio9Cest
         $I->click('Enviar');
         
     }
+    public function Exercicio9TestValidInput(AcceptanceTester $I)
+    {
+        $I->amOnPage('/exercicio9'); 
+        $I->see('Digite o número de vezes que deseja imprimir a palavra "SOL":');
+        $I->fillField('#quantidade', '5'); 
+        $I->click('Enviar');
+        $I->see('Resultado:');
+        $I->see('SOL SOL SOL SOL SOL');
+    }
+
+    public function Exercicio9TestInvalidInput(AcceptanceTester $I)
+    {
+        $I->amOnPage('/exercicio9'); 
+        $I->see('Digite o número de vezes que deseja imprimir a palavra "SOL":');
+        $I->fillField('#quantidade', '0'); 
+        $I->click('Enviar');
+        $I->see('O número deve ser maior ou igual a 1.');
+    }
 }
