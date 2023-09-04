a7f3cb41ca9cec84da0d06ec2345e887f0ef3ca4
Erica Lima <ericalimasilva570@gmail.com>
2023-09-04 18:28:01 -0300

    Conflito no test

--- tests/Acceptance/exercicio6Cest.php
+++ tests/Acceptance/exercicio6Cest.php
@@ -19,6 +19,8 @@ class exercicio6Cest
         //Eu vejo "Exercicio 6" em h1
         $I->seeInTitle('Exercício 6');
     }
+
+    
     public function formExercicio6Test(AcceptanceTester $I)
     {
         /**
@@ -28,30 +30,70 @@ class exercicio6Cest
          * mês com este número
          */
         $I->amOnPage('/exercicio6');
-        $I->see("Digite um valore entre 1 e 12");
+        
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","1");
         $I->click("Enviar");
+        $I->see("Janeiro");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","2");
         $I->click("Enviar");
+        $I->see("Fevereiro");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","3");
         $I->click("Enviar");
+        $I->see("Março");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","4");
         $I->click("Enviar");
+        $I->see("Abril");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","5");
         $I->click("Enviar");
+        $I->see("Maio");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","6");
         $I->click("Enviar");
+        $I->see("junho");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","7");
         $I->click("Enviar");
+        $I->see("Julho");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","8");
         $I->click("Enviar");
+        $I->see("Agosto");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","9");
         $I->click("Enviar");
+        $I->see("Setembro");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","10");
         $I->click("Enviar");
+        $I->see("Outubro");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","11");
         $I->click("Enviar");
+        $I->see("Novembro");
+
+        $I->see("Digite um valor entre 1 e 12");
         $I->fillField("value1","12");
         $I->click("Enviar");
+        $I->see("Dezembro");
+
+        $I->see("Digite um valor entre 1 e 12");
+        $I->fillField("value1","13");
+        $I->click("Enviar");
+        $I->see("Não corresponde a um Mês válido");
     }
 }
