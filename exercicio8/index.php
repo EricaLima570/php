aeab59dbc8da322abbccfefb0be526617325371d
Erica Lima <ericalimasilva570@gmail.com>
2023-09-07 23:19:07 -0300

    Resolvendo conflito no exercicio

--- exercicio8/index.php
+++ exercicio8/index.php
@@ -5,13 +5,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=drive-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title> exercicio 8 </title>
+    <title>Exercício 8 </title>
 </head>
 <body>
     <form action="" method="get">
-        <label for="numero"> Digite um número:</label>
-        <input type="number" name="numero">
+        <label for="numero"> Digite um número:  </label>
+        <input type="number" id="numero" name="numero">
         <button type="submit">Enviar</button>
     </form>
+<?php
+    if (isset($_GET['numero'])) {
+        $numero = intval($_GET['numero']);
+
+        if ($numero >= 1) {
+            echo "<h2>Números de 1 até $numero:</h2>";
+            for ($i = 1; $i <= $numero; $i++) {
+                echo "$i ";
+            }
+
+            $produto = 1;
+            for ($i = 1; $i <= $numero; $i++) {
+                $produto *= $i;
+            }
+
+            echo "<h2>Produto dos números de 1 até $numero:</h2>";
+            echo $produto;
+        } else {
+            echo "<p>O número deve ser maior ou igual a 1.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
