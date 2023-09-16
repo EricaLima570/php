b594305df0df375eb945d3e69ed0ed82f3d7cef2
Erica Lima <ericalimasilva570@gmail.com>
2023-09-15 21:52:53 -0300

    Refazendo teste sem utilizar inputprovider

--- tests/Acceptance/exercicio8Cest.php
+++ tests/Acceptance/exercicio8Cest.php
@@ -11,35 +11,45 @@ class exercicio8Cest
     {
     }
     
-    public function TitlExercicio8Test(AcceptanceTester $I)
+    public function TitleExercicio8Test(AcceptanceTester $I)
     {
         $I->amOnPage('/exercicio8');
         $I->seeInTitle('Exercício 8');
     }
     
-    public function testExercicio8(AcceptanceTester $I, Example $data)
+    public function testExercicio8Input5(AcceptanceTester $I)
     {
+        $input = 5;
         $I->amOnPage('/exercicio8');
         $I->see('Digite um número:');
-        $I->fillField('#numero', $data['input']);
+        $I->fillField('#numero', $input);
         $I->click('Enviar');
-
-        if ($data['input'] >= 1) {
-            $I->see("Números de 1 até {$data['input']}:");
-            $I->see($data['result']); // Verifica os números listados
-            $I->see("Produto dos números de 1 até {$data['input']}:");
-            $I->see($data['product']); // Verifica o produto calculado
-        } else {
-            $I->see("O número deve ser maior ou igual a 1.");
-        }
+        
+        $I->see("Números de 1 até {$input}:");
+        $I->see('1 2 3 4 5'); // Verifica os números listados
+        $I->see('Produto dos números de 1 até 5:');
+        $I->see('120'); // Verifica o produto calculado
     }
 
-    protected function inputProvider()
+    public function testExercicio8Input0(AcceptanceTester $I)
     {
-        return [
-            ['input' => 5, 'result' => '1 2 3 4 5', 'product' => '120'],
-            ['input' => 0, 'result' => '0', 'product' => '1'], // Corrigido o produto para '1' quando o input é 0
-            ['input' => -3, 'result' => '0', 'product' => '1'], // Corrigido o produto para '1' quando o input é -3
-        ];
+        $input = 0;
+        $I->amOnPage('/exercicio8');
+        $I->see('Digite um número:');
+        $I->fillField('#numero', $input);
+        $I->click('Enviar');
+        
+        $I->see('O número deve ser maior ou igual a 1.');
+    }
+
+    public function testExercicio8InputNegative(AcceptanceTester $I)
+    {
+        $input = -3;
+        $I->amOnPage('/exercicio8');
+        $I->see('Digite um número:');
+        $I->fillField('#numero', $input);
+        $I->click('Enviar');
+        
+        $I->see('O número deve ser maior ou igual a 1.');
     }
 }
