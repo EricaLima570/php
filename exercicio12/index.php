7650d33189256b1d5891400d01d84f0d9405a27b
Erica Lima <ericalimasilva570@gmail.com>
2023-09-24 16:11:57 -0300

    Enviando exercicio front do exercicio 12 com index e test

---
+++ exercicio12/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio12/style.css">
+    <title>Exercício 12</title>
+</head>
+<body>
+    <h1>Imprimir Palavra</h1>
+    <form action="index.php" method="post">
+        <label for="palavra">Digite uma palavra: </label>
+        <input type="text" id="palavra" name="palavra">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+
+
+
+
+
