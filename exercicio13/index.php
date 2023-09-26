048f887abdcdebb44494afabe16dd79df65f00bb
Erica Lima <ericalimasilva570@gmail.com>
2023-09-25 23:54:24 -0300

    Inserindo exercicio 13

---
+++ exercicio13/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/exercicio13/style.css">
+    <title>Exercício 13</title>
+</head>
+<body>
+    <h1>Contador de Números entre 100 e 200</h1>
+    <form action="index.php" method="post">
+        <label for="number">Digite um número (caso deseje encerrar ,digite 0):</label>
+        <input type="number" name="number" id="number" required>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
