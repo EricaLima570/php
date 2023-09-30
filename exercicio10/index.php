aa252a0495850bc63ccd7e44a9ea9a22387de234
Erica Lima <ericalimasilva570@gmail.com>
2023-09-30 15:10:34 -0300

    Resolvendo conflito no indice

--- exercicio10/index.php
+++ exercicio10/index.php
@@ -14,5 +14,31 @@
         <input type="text" id="value" name="value">
         <input type="submit" value="Enviar">
     </form>
+    <?php
+    if (isset($_POST["value"])) {
+        $value = $_POST["value"];
+        $numbers = explode(",", $value);
+
+        // Remove espaços em branco adicionais e filtra apenas os valores numéricos
+        $filteredNumbers = array_filter(array_map('trim', $numbers), 'is_numeric');
+
+        // Calcula a soma dos números positivos
+        $positiveNumbers = array_filter($filteredNumbers, function ($number) {
+            return $number > 0;
+        });
+
+        $sumPositives = array_sum($positiveNumbers);
+
+        // Conta o total de números negativos
+        $negativeNumbers = array_filter($filteredNumbers, function ($number) {
+            return $number < 0;
+        });
+
+        $countNegatives = count($negativeNumbers);
+
+        echo "<p>A soma dos números positivos é: $sumPositives</p>";
+        echo "<p>O total de números negativos é: $countNegatives</p>";
+    }
+    ?>
 </body>
 </html>
