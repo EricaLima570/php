290154957bca2164f6e5cdab8d5d0bffbef47167
Erica Lima <ericalimasilva570@gmail.com>
2023-09-30 15:10:34 -0300

    Resolvendo conflito no indice

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
         <li><a href="./exercicio7/" target="_blank">Exercicio 7</a></li>
         <li><a href="./exercicio8/" target="_blank">Exercicio 8</a></li>
         <li><a href="./exercicio9/" target="_blank">Exercicio 9</a></li>
+        <li><a href="./exercicio10/" target="_blank">Exercicio 10</a></li>
     </ul>    
 </body>
 </html>
