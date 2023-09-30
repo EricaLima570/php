d8e7446691a17b435c9edc0390e71ecb84ab517c
Erica Lima <ericalimasilva570@gmail.com>
2023-09-30 15:38:06 -0300

    Conflito indice

--- tests/Acceptance/exercicio10Cest.php
+++ tests/Acceptance/exercicio10Cest.php
@@ -3,6 +3,7 @@
 
 namespace Tests\Acceptance;
 
+use Codeception\Template\Acceptance;
 use Tests\Support\AcceptanceTester;
 
 class exercicio10Cest
@@ -18,5 +19,16 @@ class exercicio10Cest
         $I->see("Exercicio 10");
         $I->fillField("value","1,2,-3,6,-20,8,-4,30,56,3,-1,5,7,9,-34,-6,-7,-36,-70,47");
         $I->click('Enviar');
+    }
+    public function Exercicio10backTest(AcceptanceTester $I){
+        $I->amOnPage("/Exercicio10"); // Altere esta URL conforme necessário
+
+        // Insira 20 números separados por vírgula no campo de entrada
+        $I->fillField('#value', '1,2,-3,4,-5,6,7,-8,9,-10,11,12,-13,14,-15,16,17,-18,19,-20');
+        $I->click('Enviar');
+
+        // Verifique os resultados esperados
+        $I->see('A soma dos números positivos é: 118');
+        $I->see('O total de números negativos é: 8');
     }
 }
