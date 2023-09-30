a94b2cc4d53981a114cc1a7c6434ebfa6e9882a3
Erica Lima <ericalimasilva570@gmail.com>
2023-09-30 15:39:07 -0300

    Adicionando exercicio 13 ao indice

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <li><a href="./exercicio10/" target="_blank">Exercicio 10</a></li>
         <li><a href="./exercicio11/" target="_blank">Exercicio 11</a></li>
         <li><a href="./exercicio12/" target="_blank">Exercicio 12</a></li>
+        <li><a href="./exercicio13/" target="_blank">Exercicio 13</a></li>
     </ul>    
 </body>
 </html>
